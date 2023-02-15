0f2cfaaf0d9cbe88c1d750ca34049e0ab6c1a915
Your Name <you@example.com>
2023-02-15 13:14:11 +0000

    ..

--- schemas.go
+++ schemas.go
@@ -490,7 +490,7 @@ type Apps struct {
 	AppID          string         `src:"appID" dest:"app_id"`
 	UserID         string         `src:"userID" dest:"user_id" fkey:"users,user_id"`
 	Position       string         `src:"position" dest:"position"`
-	ReviewFeedback string         `src:"review_feedback" dest:"review_feedback" notnull:"true" notnull:"true"`
+	ReviewFeedback string         `src:"review_feedback,omitempty" dest:"review_feedback" default:"null"`
 	CreatedAt      time.Time      `src:"createdAt" dest:"created_at" default:"NOW()"`
 	Questions      map[string]any `src:"questions" dest:"questions" default:"{}"`
 	Answers        map[string]any `src:"answers" dest:"answers" default:"{}"`
